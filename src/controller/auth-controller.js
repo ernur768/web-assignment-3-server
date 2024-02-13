2345c0aeac4dfd518910a354f4d1d6e8289e1983
ernur <220776@astanait.edu.kz>
2024-02-13 12:42:50 +0600

    feat: add authentication controller

---
+++ src/controller/auth-controller.js
@@ -0,0 +1,69 @@
+const authService = require("../service/auth-service");
+
+class AuthController {
+    async register(req, res, next) {
+        try {
+            const { email, password, username } = req.body
+            const {user} = await authService.register(email, password, username)
+
+            res.status(200).json(user)
+        }
+        catch (error) {
+            next(error)
+        }
+    }
+
+    async login(req, res, next) {
+        try {
+            const { email, password } = req.body
+            const user = await authService.login(email, password)
+            res.cookie(
+                'refreshToken',
+                user.refreshToken,
+                {
+                    httpOnly: true,
+                    maxAge: process.env.REFRESH_TOKEN_COOKIE_MAX_AGE_DAY * 24 * 60 * 60 * 1000
+                }
+            )
+
+            res.status(200).json(user)
+        }
+        catch (error) {
+            next(error)
+        }
+    }
+
+    async logout(req, res, next) {
+        try {
+            const { refreshToken } = req.cookies
+            const token = await authService.logout(refreshToken)
+            res.clearCookie('refreshToken')
+            res.status(200).json(token)
+        }
+        catch (error) {
+            next(error)
+        }
+    }
+
+    async refresh(req, res, next) {
+        try {
+            const { refreshToken } = req.cookies
+            const user = await authService.refresh(refreshToken)
+            res.cookie(
+                'refreshToken',
+                user.refreshToken,
+                {
+                    httpOnly: true,
+                    maxAge: process.env.REFRESH_TOKEN_COOKIE_MAX_AGE_DAY * 24 * 60 * 60 * 1000
+                }
+            )
+
+            res.status(200).json(user)
+        }
+        catch (error) {
+            next(error)
+        }
+    }
+}
+
+module.exports = new AuthController();
